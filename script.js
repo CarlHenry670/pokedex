88251e268ac6e2926b47974342227b120139c222
CarlHenry670 <capic.2023118tads0035@aluno.ifpi.edu.br>
2024-02-15 16:26:10 -0300

    segunda tipagem adicionada

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const colors = {
     grass: '#98D2A3',
     electric: '#FFE082',
     water: '#87CEEB',
-    ground: '#D2691E',  // Um pouquinho mais escura para ground
-    rock: '#D2B48C',     // Marrom clarinho para rock
+    ground: '#D2691E',  
+    rock: '#D2B48C',     
     fairy: '#fceaff',
     poison: '#D8BFD8',
     bug: '#D8F8D8',
@@ -16,7 +16,7 @@ const colors = {
     flying: '#ADD8E6',
     fighting: '#CD5C5C',
     normal: '#F5F5F5',
-    ice: '#B0E0E6',       // Cor de gelo para ice
+    ice: '#B0E0E6',       
     ghost: '#7b62a3',
     steel: '#B8B8D0',
     dark: '#708090'
@@ -56,27 +56,32 @@ const getPokemon = async (id) => {
 const createPokeCard = (poke) => {
     const pokeEl = document.createElement('div');
     pokeEl.classList.add('pokemon');
+
     const name = poke.name[0].toUpperCase() + poke.name.slice(1);
     const id = poke.id.toString().padStart(3, '0');
-    const pokeType = poke.types.map(type => type.type.name);
-    const type = pokeTypes.find(type => pokeType.indexOf(type) > -1);
-    const color = colors[type];
-    pokeEl.style.backgroundColor = color;
-    const pokeInnerHTML = `
-    <div class="pokeImg">
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${poke.id}.png"
-        alt="${name}">
-</div>
 
-<div class="upon">
-    <span class="number">#${id}</span>
-    <h2 class="name">${name}</h2>
-    <span class="type">Tipo: ${type}</span>
-</div>
+    const firstType = poke.types[0].type.name;
+    pokeEl.style.backgroundColor = colors[firstType];
+    const allTypes = poke.types.map(type => type.type.name);
+    
+    const pokeInnerHTML = `
+        <div class="pokeImg">
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${poke.id}.png" alt="${name}">
+        </div>
+
+        <div class="upon">
+            <span class="number">#${id}</span>
+            <h2 class="name">${name}</h2>
+            <span class="type">Tipo: ${allTypes.join('/')}</span>
+        </div>
     `;
+
     pokeEl.innerHTML = pokeInnerHTML;
     pokeBox.appendChild(pokeEl);
 }
+
+
+
 
 const searchPokemon = () => {
     const searchTerm = document.getElementById('searchInput').value.toLowerCase();
